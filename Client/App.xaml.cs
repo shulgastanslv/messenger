afbfe3b059b0c78d337a3891f34bd26f97248134
akiroqw <akiroqw@gmail.com>
2023-05-11 13:08:59 +0300

    initialize-project

---
+++ Client/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client
+{
+    public partial class App
+    {
+        private void ApplicationStart(object sender, StartupEventArgs e)
+        {
+        }
+    }
+}
